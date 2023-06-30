b2ab9a1ac153b87fcbbd494d44298ab879f283c3
pollah yves <yvesfranky961@gmail.com>
2023-06-30 14:12:16 +0100

    creation du model(table de la bd) et enregistrement du model dans la base de donnes

--- BLOG/blog/models.py
+++ BLOG/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+class CreateBlog(models.Model):
+    titre = models.CharField(max_length=200)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='media')
+    date_added = models.DateTimeField(auto_now=True)
+    
+    #pour classer les poste en fonction des date les plus recent
+    class Meta:
+        ordering = ['-date_added']
+        
+class Comment(models.Model):
+    post = ForeignKey(CreateBlog,related_name='comments',on_delete=models.CASCADE)
+    email = models.EmailField()
+    body = models.TextField()
+    name = models.CharField(max_length=100,default="inconnu") 
+    date_added = models.DateTimeField(auto_now=True)
+    
+    #pour classer les commentaire en fonction des date les plus recent
+    class Meta:
+        ordering = ['-date_added']
+            
--- BLOG/blog/views.py
+++ BLOG/blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView # puisque on va utiliser la pagination pour les differant page
 
-# Create your views here.
+# class List(ListView):
+#     template_name='blog/index.html'
 
+def index(request):
+    return render(request,'blog/index.html')
+    
